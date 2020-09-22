e7af6a1626e2297f560589f59469b41268399d18
shirishphatangare <sphatangare@gmail.com>
2020-09-22 17:52:38 -0500

    Adding Linux commands and Linux Shell scripting notes and files

---
+++ Files/sample/find_utility/create_sample_files.sh
@@ -0,0 +1,12 @@
+touch MybashProgram.sh
+touch mycprogram.c
+touch MyCProgram.c
+touch Program.c
+
+mkdir backup
+cd backup
+
+touch MybashProgram.sh
+touch mycprogram.c
+touch MyCProgram.c
+touch Program.c
